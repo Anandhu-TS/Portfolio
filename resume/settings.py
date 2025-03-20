6fe8539efd36a42942264389a6406a8f7e239a1d
Anandhu-TS <anandhutsarun@gmail.com>
2025-03-20 23:53:16 +0530

    Fixed ALLOWED_HOSTS and DEBUG settings

--- resume/settings.py
+++ resume/settings.py
@@ -23,14 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#dzxdtor)3!^u$+)3-941p)955#1)5+2!3b+0mp1wli*9c10)1'
+# SECRET_KEY = 'django-insecure-#dzxdtor)3!^u$+)3-941p)955#1)5+2!3b+0mp1wli*9c10)1'
+SECRET_KEY = os.getenv('SECRET_KEY', '2626')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['your-app-name.onrender.com']
+ALLOWED_HOSTS = ['portfolio-8zlt.onrender.com', '127.0.0.1', 'localhost']
 
 
 
